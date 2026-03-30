2671ad2e3be53b079ddff7fe5ef1cc12eff508b5
Daniel Geymayer <daniel@geymayer.com>
2026-03-30 20:40:36 +0200

    Also write current CO2 level to file (preferably in tmpfs directory), add print statements

--- co2.py
+++ co2.py
@@ -14,8 +14,16 @@ config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 date = datetime.now(tz.tzlocal()).isoformat()
 co2 = int(mh_z19.read()['co2'])
 
+print(f"CO2: {co2} ppm")
+
 url = config['co2']['api_url']
 data = { 'fields': [date, co2] }
 headers = { 'Content-Type': 'application/json' }
 
 requests.post(url, data=json.dumps(data), headers=headers)
+print(f"CO2 level sent to API {url}")
+
+co2_filepath = config['co2']['co2_filepath']
+with open(co2_filepath, 'w') as writer:
+    writer.write(str(co2))
+    print(f"CO2 level written to {co2_filepath}")
